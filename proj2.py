6211b597891da3cd5b7217ef0466d44d850d6920
soumil-mukherjee <soumil.mukherjee0310@gmail.com>
2026-09-19 20:40:16 +0530

    Add files via upload

---
+++ proj2.py
@@ -0,0 +1,18 @@
+from google import genai
+import logging
+
+logging.getLogger("google_genai").setLevel(logging.ERROR)
+logging.getLogger("google_genai.models").setLevel(logging.ERROR) 
+ #find your api key from places such as google AI studio
+client= genai.Client(api_key="Your API key") 
+ 
+while True: 
+    question=input("you: ") 
+ 
+    if question.lower() ==  "exit":break 
+ 
+    response= client.models.generate_content( 
+        model="gemini-3.6-flash",contents=question 
+   ) 
+ 
+    print("gemini",response.text) 
